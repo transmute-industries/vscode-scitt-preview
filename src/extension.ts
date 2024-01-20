717d305062a8b21aad41e9ce399a015900503202
Orie Steele <orie@transmute.industries>
2024-01-20 08:50:19 -0600

    add cbor copy of pawdraw

--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,10 @@
 import * as vscode from 'vscode';
 import { CatScratchEditorProvider } from './catScratchEditor';
 import { PawDrawEditorProvider } from './pawDrawEditor';
-
+import { CborEditorProvider } from './cborEditor';
 export function activate(context: vscode.ExtensionContext) {
 	// Register our custom editor providers
 	context.subscriptions.push(CatScratchEditorProvider.register(context));
 	context.subscriptions.push(PawDrawEditorProvider.register(context));
+	context.subscriptions.push(CborEditorProvider.register(context));
 }
